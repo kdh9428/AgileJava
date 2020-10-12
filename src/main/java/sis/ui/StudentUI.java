876f21495ba7747983fb4fafbaedc28b8b30c9f0
dahunkim <kim23222@naver.com>
2020-10-12 23:12:41 +0900

    [추가] 학생 UI 코드 추가

--- src/main/java/sis/ui/StudentUI.java
+++ src/main/java/sis/ui/StudentUI.java
@@ -4,6 +4,7 @@ import sis.studentinfo.Student;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,12 +27,36 @@ public class StudentUI {
         this.writer = writer;
     }
 
-    public void run(){
+    public void run() throws IOException {
 
+        String line;
+        do {
+            write(MENU);
+            line = reader.readLine();
+            if (line.equals(ADD_OPTION))
+                addStudent();
+        }while (!line.equals(QUIT_OPTION));
+    }
+
+    private void addStudent() throws IOException {
+        write(NAME_PROMPT);
+        String name = reader.readLine();
+        students.add(new Student(name));
+        writeln(ADDED_MESSAGE);
+    }
+
+    private void writeln(String line) throws IOException {
+        write(line);
+        writer.newLine();
+        writer.flush();
+    }
+
+    private void write(String line) throws IOException {
+        writer.write(line, 0, line.length());
+        writer.flush();
     }
 
     public List<Student> getAddedStudents() {
-
         return students;
     }
 }
