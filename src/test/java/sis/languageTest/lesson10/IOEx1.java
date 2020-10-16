72be5424c5ac0641177d9d30cc714094cb9f7a4f
dahunkim <kim23222@naver.com>
2020-10-16 17:31:15 +0900

    [테스트] ByteArrayOutputStream, DataOutputStream 테스트

--- src/test/java/sis/languageTest/lesson10/IOEx1.java
+++ src/test/java/sis/languageTest/lesson10/IOEx1.java
@@ -98,5 +98,40 @@ public class IOEx1 {
         }
     }
 
+    @Test
+    public void testDataOutputStreamEx02(){
+
+        ByteArrayOutputStream bos = null;
+        DataOutputStream dos = null;
+
+        byte[]  result = null;
+
+        try {
+            bos = new ByteArrayOutputStream();
+            dos = new DataOutputStream(bos);
+            dos.writeInt(10);
+            dos.writeFloat(20.0f);
+            dos.writeUTF("test");
+            dos.writeBoolean(true);
+
+            result = bos.toByteArray();
+
+            String[] hex = new String[result.length];
+
+            for (int i = 0; i < result.length; i++){
+                if (result[i] < 0){
+                    hex[i] = String.format("%02x", result[i]+256);
+                }else {
+                    hex[i] = String.format("%02x", result[i]);
+                }
+            }
+
+            System.out.println("10진수 : "+ Arrays.toString(result));
+            System.out.println("16진수 : "+ Arrays.toString(hex));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 
 }
