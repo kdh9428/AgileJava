1515e54fb365611ae138d59cf663b47f9c226593
dahunkim <kim23222@naver.com>
2020-10-21 15:51:21 +0900

    [추가] 강의를 지원한 학생 리스트 작성, 테스트 추가

--- src/main/java/sis/studentinfo/StudentDirectory.java
+++ src/main/java/sis/studentinfo/StudentDirectory.java
@@ -1,18 +1,36 @@
 package sis.studentinfo;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class StudentDirectory {
 
     private Map<String, Student> students = new HashMap<>();
+    private static final String DIR_BASENAME = "studentDir";
+    private DataFile db;
 
-    public void add(Student student){
-        students.put(student.getId(), student);
+    public StudentDirectory() throws IOException {
+        db = DataFile.open(DIR_BASENAME);
     }
 
-    public Student findById(String id){
-        return students.get(id);
+    public void add(Student student) {
+
+        db.add(student.getId(), student);
+//        students.put(student.getId(), student);
+
     }
 
+    public Student findById(String id) {
+//        return students.get(id);
+        return (Student) db.findBy(id);
+    }
+
+    public void close() throws IOException {
+        db.close();
+    }
+
+    public void remove() {
+        db.deleteFiles();
+    }
 }
--- src/test/java/sis/studentinfo/StudentDirectoryTest.java
+++ src/test/java/sis/studentinfo/StudentDirectoryTest.java
@@ -3,31 +3,54 @@ package sis.studentinfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StudentDirectoryTest {
 
-    private StudentDirectory directory;
+    private StudentDirectory dir;
 
     @BeforeEach
     public void setUp() {
-        directory = new StudentDirectory();
+        dir = new StudentDirectory();
+    }
+
+    protected void tearDown() throws IOException {
+        dir.close();
+        dir.remove();
+    }
+
+
+    @Test
+    public void testRandomAccess() throws IOException {
+        final int numberOfStudents = 10;
+        for (int i = 0; i <numberOfStudents; i++){
+            addStudent(dir, i);
+        }
+
+        dir.close();
+
+        dir = new StudentDirectory();
+        for (int i = 0; i <numberOfStudents; i++){
+            verifyStudentLookup(dir ,i);
+        }
     }
 
     @Test
-    public void testStoreAndRetrieve() {
+    public void testStoreAndRetrieve() throws IOException {
         final int numberOfStudents = 10;
 
         for (int i = 0; i < numberOfStudents; i++) {
-            addStudent(directory, i);
+            addStudent(dir, i);
         }
 
         for (int i = 0; i < numberOfStudents; i++) {
-            verifyStudentLookup(directory, i);
+            verifyStudentLookup(dir, i);
         }
     }
 
-    private void verifyStudentLookup(StudentDirectory directory, int i) {
+    private void verifyStudentLookup(StudentDirectory directory, int i) throws IOException{
         String id = "" + i;
         Student student =directory.findById(id);
         assertEquals(id, student.getLastName());
@@ -35,7 +58,7 @@ class StudentDirectoryTest {
         assertEquals(i, student.getCredits());
     }
 
-    private void addStudent(StudentDirectory directory, int i) {
+    private void addStudent(StudentDirectory directory, int i) throws IOException{
         String id = "" + i;
         Student student = new Student(id);
 
