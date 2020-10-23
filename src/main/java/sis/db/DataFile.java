9a6d4a3b82cf25860549a7f652a04b89d8019e37
dahunkim <kim23222@naver.com>
2020-10-23 23:55:25 +0900

    [추가] 데이터 파일 생성 코드 작성

--- src/main/java/sis/db/DataFile.java
+++ src/main/java/sis/db/DataFile.java
@@ -1,4 +1,99 @@
 package sis.db;
 
+import java.io.*;
+
 public class DataFile {
+
+    public static final String DATA_EXT = ".db";
+    public static final String KEY_EXT = ".idx";
+
+    private String dataFilename;
+    private String keyFilename;
+
+    private RandomAccessFile db;
+    private KeyFile keys;
+
+    public static DataFile create(String filebase) throws IOException {
+
+        return new DataFile(filebase, true);
+    }
+
+    public static DataFile open(String filebase) throws IOException {
+        return new DataFile(filebase, false);
+    }
+
+    public DataFile(String filebase, boolean deleteFiles) throws IOException {
+
+        dataFilename = filebase + DATA_EXT;
+        keyFilename = filebase + KEY_EXT;
+
+        if (deleteFiles)
+            deleteFiles();
+        openFiles();
+    }
+
+    public void add(String key, Object object) throws IOException {
+
+        long position = db.length();
+
+        byte[] bytes = getBytes(object);
+        db.seek(position);
+        db.write(bytes, 0, bytes.length);
+
+        keys.add(key, position, bytes.length);
+    }
+
+    public Object findBy(String id) throws IOException {
+        if (!keys.containsKey(id)) {
+            return null;
+        }
+
+        long position = keys.getPosition(id);
+        db.seek(position);
+
+        int length = keys.getLength(id);
+        return read(length);
+    }
+
+    public int size() {
+        return keys.size();
+    }
+
+    public void close() throws IOException {
+        keys.close();
+        db.close();
+    }
+
+    public void deleteFiles() {
+        IOUtil.delete(dataFilename, keyFilename);
+    }
+
+    private Object readObject(byte[] bytes) throws IOException {
+        ObjectInputStream input = new ObjectInputStream(new ByteArrayInputStream(bytes));
+
+        try {
+            try {
+                return input.readObject();
+            } catch (ClassNotFoundException unlikely) {
+                //필요하다면 테스트 작성
+                return null;
+            }
+        } finally {
+            input.close();
+        }
+    }
+
+    private void openFiles() throws IOException {
+        keys = new KeyFile(keyFilename);
+        db = new RandomAccessFile(new File(dataFilename), "rw");
+    }
+
+    private byte[] getBytes(Object object) throws IOException {
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        ObjectOutputStream outputStream = new ObjectOutputStream(byteStream);
+        outputStream.writeObject(object);
+        outputStream.flush();
+        return byteStream.toByteArray();
+    }
+
 }
--- src/test/java/sis/studentinfo/StudentDirectoryTest.java
+++ src/test/java/sis/studentinfo/StudentDirectoryTest.java
@@ -25,15 +25,15 @@ class StudentDirectoryTest {
     @Test
     public void testRandomAccess() throws IOException {
         final int numberOfStudents = 10;
-        for (int i = 0; i <numberOfStudents; i++){
+        for (int i = 0; i < numberOfStudents; i++) {
             addStudent(dir, i);
         }
 
         dir.close();
 
         dir = new StudentDirectory();
-        for (int i = 0; i <numberOfStudents; i++){
-            verifyStudentLookup(dir ,i);
+        for (int i = 0; i < numberOfStudents; i++) {
+            verifyStudentLookup(dir, i);
         }
     }
 
@@ -50,15 +50,15 @@ class StudentDirectoryTest {
         }
     }
 
-    private void verifyStudentLookup(StudentDirectory directory, int i) throws IOException{
+    private void verifyStudentLookup(StudentDirectory directory, int i) throws IOException {
         String id = "" + i;
-        Student student =directory.findById(id);
+        Student student = directory.findById(id);
         assertEquals(id, student.getLastName());
         assertEquals(id, student.getId());
         assertEquals(i, student.getCredits());
     }
 
-    private void addStudent(StudentDirectory directory, int i) throws IOException{
+    private void addStudent(StudentDirectory directory, int i) throws IOException {
         String id = "" + i;
         Student student = new Student(id);
 
