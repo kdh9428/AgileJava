0da5aa8d5c750c5538ebb8e639d7f8d335532bed
dahunkim <kim23222@naver.com>
2020-10-28 22:27:52 +0900

    [추가] 파일 삭제 유틸 작성

--- src/main/java/sis/ui/IOUtil.java
+++ src/main/java/sis/ui/IOUtil.java
@@ -1,4 +1,17 @@
 package sis.ui;
 
+import java.io.File;
+
 public class IOUtil {
+
+    public static boolean delete(String... filenames){
+        boolean deleteAll = true;
+
+        for (String filename : filenames){
+            if (!new File(filename).delete()){
+                deleteAll = false;
+            }
+        }
+            return deleteAll;
+    }
 }
