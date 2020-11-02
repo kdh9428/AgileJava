ebb818cfb14056eb6b7dae57279ad2a59049f525
dahunkim <kim23222@naver.com>
2020-11-02 16:23:41 +0900

    [테스트] 테스트를 하기 위해 Mock 객체 생성

---
+++ src/test/java/sis/studentinfo/MockAch.java
@@ -0,0 +1,48 @@
+package sis.studentinfo;
+
+import com.jimbob.*;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MockAch implements Ach {
+    @Override
+    public AchResponse issueDebit(AchCredentials achCredentials, AchTransactionData data) {
+        assertTrue(data.account.equals(AccountTest.ACCOUNT_NUMBER));
+
+        assertTrue(data.aba.equals(AccountTest.ABA));
+
+        AchResponse response = new AchResponse();
+        response.timestamp = new Date();
+        response.traceCode = "1";
+
+        response.status = AchStatus.SUCCESS;
+        return response;
+    }
+
+    @Override
+    public AchResponse markTransactionAsNSF(AchCredentials achCredentials, AchTransactionData data, String traceCode) {
+        return null;
+    }
+
+    @Override
+    public AchResponse refundTransaction(AchCredentials credentials, AchTransactionData data, String traceCode) {
+        return null;
+    }
+
+    @Override
+    public AchResponse issueCredit(AchCredentials credentials, AchTransactionData data) {
+        return null;
+    }
+
+    @Override
+    public AchResponse voidSameDayTransaction(AchCredentials credentials, AchTransactionData data, String traceCode) {
+        return null;
+    }
+
+    @Override
+    public AchResponse queryTransactionStatus(AchCredentials credentials, AchTransactionData data, String traceCode) {
+        return null;
+    }
+}
