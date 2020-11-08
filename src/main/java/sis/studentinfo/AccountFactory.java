87df14a8633b19f3be3d284f46d024eee9ce8652
dahunkim <kim23222@naver.com>
2020-11-08 17:25:43 +0900

    [추가] Accountable 인터페이스 생성, Account에서 구현

---
+++ src/main/java/sis/studentinfo/AccountFactory.java
@@ -0,0 +1,22 @@
+package sis.studentinfo;
+
+import java.lang.reflect.Proxy;
+
+public class AccountFactory {
+
+    public static Accountable create(Permission permission){
+
+        switch (permission){
+            case UPDATE :
+                return new Account();
+            case READ_ONLY:
+                return createSecuredAccount();
+        }
+        return null;
+    }
+
+    private static Accountable createSecuredAccount(){
+        SecureProxy secureAccount = new SecureProxy(new Account(), "credit", "setBankAba", "setBankAccountNumber", "setBankAccountType", "transferFromBank");
+        return (Accountable) Proxy.newProxyInstance(Accountable.class.getClassLoader(), new Class[]{Accountable.class}, secureAccount);
+    }
+}
