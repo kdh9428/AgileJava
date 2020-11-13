8cc856d50ad67826a8b77bfe4a1d9107dcea75d9
DaHun <kim23222@naver.com>
2020-11-13 17:02:11 +0900

    [테스트] 멀티쓰레딩 검색 서버 테스트 정상적인 검색, 검색 에러 테스트 추가

--- src/test/java/sis/search/SearchTest.java
+++ src/test/java/sis/search/SearchTest.java
@@ -2,9 +2,10 @@ package sis.search;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SearchTest {
 
@@ -13,14 +14,25 @@ class SearchTest {
     @Test
     public void testCreate() throws IOException {
         Search search = new Search(URL, "x");
-
         assertEquals(URL, search.getUrl());
         assertEquals("x", search.getText());
     }
 
     @Test
     public void testPositiveSearch() throws IOException {
-
+        Search search = new Search(URL, "Jeff Langr");
+        search.execute();
+        assertTrue(search.matches() >= 1);
+        assertFalse(search.errored());
     }
 
+    @Test
+    public void testErroredSearch() throws IOException {
+
+        final String badUrl = URL + "/z2468.html";
+        Search search = new Search(badUrl, "whatever");
+        search.execute();
+        assertTrue(search.errored());
+        assertEquals(FileNotFoundException.class, search.getError().getClass());
+    }
 }
