13b923621422a81bf6da5d49ebc69af39d9b4ce5
DaHun <kim23222@naver.com>
2020-11-18 20:22:15 +0900

    [추가] StringUtil occurrences() 문자열 길이를 비교해 입력받은 문자열보다 클 경우 regionMatches()로 같은 문자열 찾는 유틸

--- src/main/java/sis/util/StringUtil.java
+++ src/main/java/sis/util/StringUtil.java
@@ -10,4 +10,16 @@ public class StringUtil {
     public static String appendNewLine(String str) {
         return str + NEWLINE;
     }
+
+    public static int occurrences(String string, String subString) {
+        int occurrences = 0;
+        int length = subString.length();
+        final boolean ignoreCase = true;
+
+        for (int i = 0; i < string.length() - subString.length() + 1; i++) {
+            if (string.regionMatches(ignoreCase, i, subString, 0, length))
+                occurrences++;
+        }
+        return occurrences;
+    }
 }
