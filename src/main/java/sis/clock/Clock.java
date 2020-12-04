47d1f8a2454729c54959ae6c72b4b24fd8f35478
dahunkim <kim23222@naver.com>
2020-12-04 21:22:43 +0900

    [테스트] 스레드 interrupt 호출, 스레드 exception으로 다시 시작되는 테스트 작성

---
+++ src/main/java/sis/clock/Clock.java
@@ -0,0 +1,44 @@
+package sis.clock;
+
+import java.util.Date;
+
+public class Clock implements Runnable {
+
+    private ClockListener listener;
+    private boolean run = true;
+
+    public Clock(ClockListener listener) {
+        this.listener = listener;
+        new Thread(this).start();
+    }
+
+    public void stop() {
+        run = false;
+    }
+
+    @Override
+    public void run() {
+
+        long lastTime = System.currentTimeMillis();
+        System.out.println(lastTime);
+        while (run) {
+            try {
+                Thread.sleep(1000);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            long now = System.currentTimeMillis();
+
+
+            long l = (now / 1000) - (lastTime / 1000);
+            System.out.println("시간측정: " + now);
+            System.out.println("계산 : "+ l);
+            if ((now / 1000) - (lastTime / 1000) >= 1){
+                listener.update(new Date(now));
+                lastTime = now;
+            }
+        }
+    }
+}
